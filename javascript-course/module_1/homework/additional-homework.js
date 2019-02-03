d5ecb1b0fe9314e3d4796a76054f855833f72af2
Bohdan Kucheriavyi <bohdank911@gmail.com>
2019-02-03 15:39:15 +0200

    Module 1. Additional homework

--- javascript-course/module_1/homework/additional-homework.js
+++ javascript-course/module_1/homework/additional-homework.js
@@ -3,14 +3,55 @@
 let sharm = 15;
 let hurgada = 25;
 let taba = 6;
+let groupSharm = 'sharm';
+let groupHurgada = 'hurgada';
+let groupTaba = 'taba';
+let places = 'Есть место в групе ';
+let noPlaces = 'Извините, столько мест нет ни в одной группе!';
+let doYouWant = ' Согласны ли Вы быть в этой групе?';
+let okGroup = 'Приятного путешествия в группе: ';
 let userNumber = prompt('Введите число необходимых мест');
+let intCheck = Number.parseInt(userNumber);
 const enterError = 'Ошибка ввода';
 const comeAgain = 'Нам очень жаль, приходите еще!';
 
-if (userNumber === null){
+if (userNumber === null) {
     alert(comeAgain);
-} else if (isNaN(userNumber) === true || userNumber < 1){
+} else if (isNaN(userNumber) === true || userNumber < 1 || userNumber === intCheck) {
     alert(enterError);
-} else{
-    // 
+} else if (userNumber > sharm && userNumber > hurgada && userNumber > taba) {
+    alert(noPlaces);
+} else {
+    if (userNumber <= sharm && userNumber > taba) {
+        let isSharm = confirm(`${places} ${groupSharm} . ${doYouWant}`);
+        if (isSharm === true) {
+            alert(`${okGroup} ${groupSharm}`);
+        } else {
+            alert(comeAgain);
+        }
+    } else if (userNumber === false) {
+        alert(noPlaces);
+    }
+
+    if (userNumber <= hurgada && userNumber > sharm) {
+        let isHurgada = confirm(`${places} ${groupHurgada} . ${doYouWant}`);
+        if (isHurgada === true) {
+            alert(`${okGroup} ${groupHurgada}`);
+        } else {
+            alert(comeAgain);
+        }
+    } else if (userNumber === false) {
+        alert(noPlaces);
+    }
+
+    if (userNumber <= taba && userNumber < taba++) {
+        let isTaba = confirm(`${places} ${groupTaba} . ${doYouWant}`);
+        if (isTaba === true) {
+            alert(`${okGroup} ${groupTaba}`);
+        } else {
+            alert(comeAgain);
+        }
+    } else if (userNumber === false) {
+        alert(noPlaces);
+    }
 }
