ab9e6f81bb86374392a8e677b3ded58607fc897a
Bohdan Kucheriavyi <bohdank911@gmail.com>
2019-04-04 12:04:47 +0300

    Module 9. Lesson

--- javascript-course/module_9/lesson/script.js
+++ javascript-course/module_9/lesson/script.js
@@ -31,6 +31,27 @@ stop.addEventListener('click', t.handleStop);
 
 // =============================================================================================
 
-let date = Date;
+let date = new Date();
 
-console.log( date.parse() );
+// проверим количество мс с с начала эпохи Unix
+console.log(
+  `Время в мс с начала эпохи Unix: ${date.getTime()}`
+);
+// "Время в мс с начала эпохи Unix: ....  значения будут меняться :-)
+// "Время в мс с начала эпохи Unix: 1504721892483"
+
+let result = 'Date: ';
+result += date.getFullYear() + '/'; // год
+result += date.getMonth() + '/'; // месяц, начиная с 0
+result += date.getDate() + '. Day of the week: '; // день недели, начиная с 1
+result += date.getDay() + '.'; // день , начиная с 0
+
+result += ' Time: ' + date.getHours() + ':'; // часы
+result +=  date.getMinutes() + ':'; // минуты
+result +=  date.getSeconds() + ':'; // секунды
+result +=  date.getMilliseconds() + ''; // милисекунды
+
+// строка должна показать текущее время и дату
+console.log(result);
+//  ....  значения будут менятся :-)
+// "Date: 2017/8/6. Day of week: 3. Time: 21:18:12:483"
