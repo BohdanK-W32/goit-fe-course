09c2c1e6555852bc740ed73247f977eac6ebcad5
Bohdan Kucheriavyi <bohdank911@gmail.com>
2019-04-12 23:21:26 +0300

    Module 8. Homework (+slide buttons)

--- javascript-course/module_8/homework/script.js
+++ javascript-course/module_8/homework/script.js
@@ -8,78 +8,21 @@ const galleryItems = [
   { preview: 'img/preview-5.jpg', fullview: 'img/fullview-5.jpg', alt: "mountain road" },
   { preview: 'img/preview-6.jpg', fullview: 'img/fullview-6.jpg', alt: "lights" },
   { preview: 'img/preview-7.jpg', fullview: 'img/fullview-7.jpg', alt: "rain drop" },
-  { preview: 'img/preview-8.jpg', fullview: 'img/fullview-8.jpg', alt: "sunset lights" }
+  { preview: 'img/preview-8.jpg', fullview: 'img/fullview-8.jpg', alt: "sunset lights" },
+  { preview: 'img/preview-9.jpg', fullview: 'img/fullview-9.jpg', alt: "cubes" },
+  { preview: 'img/preview-10.jpg', fullview: 'img/fullview-10.jpg', alt: "Lamborghini Terzo Millennio" },
+  { preview: 'img/preview-11.jpg', fullview: 'img/fullview-11.jpg', alt: "Lamborghini Terzo Millennio" },
+  { preview: 'img/preview-12.jpg', fullview: 'img/fullview-12.jpg', alt: "Lamborghini Terzo Millennio" }
 ];
-
-
-function GalleryFunction () {
-  const preview = document.querySelector('.preview');
-  const img_gallery = document.querySelector('.js-image-gallery');
-
-  preview.addEventListener('click', onImgClick);
-  preview.addEventListener('mouseover', onImgOver);
-  preview.addEventListener('mouseout', onImgOut);
-  createGallery();
-
-  const previewFirstLi = preview.firstElementChild;
-  const full_img = document.querySelector('.js-fullview-img');
-
-  full_img.setAttribute ( 'src', previewFirstLi.firstElementChild.getAttribute('data-fullview') );
-
-  function createGallery () { 
-    const div = document.createElement('div');
-    const img = document.createElement('img');
-    
-    div.classList.add('fullview');
-    div.classList.add('js-fullview');
-    img.classList.add('js-fullview-img');
-    
-    galleryItems.map(el => {
-      const li = document.createElement('li');
-      const img = document.createElement('img');
-      
-      img.setAttribute( 'src', el.preview );
-      img.setAttribute( 'data-fullview', el.fullview );
-      img.setAttribute( 'alt', el.alt );
-      
-      img_gallery.append(div);
-      li.append(img);
-      preview.append(li);
-    }); 
-    
-    div.append(img);
-    img_gallery.prepend(div);
-  }
-
-  function onImgClick (e) { if ( e.target.nodeName === 'IMG') {full_img.setAttribute('src', e.target.getAttribute('data-fullview'))} }
-  function onImgOver (e) { if ( e.target.nodeName === 'IMG') {e.target.classList.add('js-current-img')} }
-  function onImgOut (e) { if ( e.target.nodeName === 'IMG') {e.target.classList.remove('js-current-img')} }
-}
-
-GalleryFunction()
-
-/*
-  ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
-  
-  Создайте плагин галлереи используя ES6 класс. Добавьте поля и методы класса так, 
-  чтобы можно было создать любое количество галлерей на странице. Функционал плагина 
-  аналогичный заданию выше.
-  
-  При создании экземпляра конструктор получает:
-    - items - список элементов для preview
-    - parentNode - ссылку на DOM-узел в который будут помещены fullview и preview
-    - defaultActiveItem - номер активного элемента preview по умолчанию
-    
-  Тогда создание экземпляра будет выглядеть следующим образом.
-*/
 
 class Gallery {
 
-  constructor ({items, parentNode, defaultActiveItem}) {
+  constructor ({items, parentNode, defaultActiveItem = 1}) {
     this._items = items, 
     this._parentNode = parentNode, 
     this._defaultActiveItem = defaultActiveItem, 
-    this._preview = document.querySelector('.preview-es6')
+    this._preview = document.querySelector('.preview-es6'),
+    this.createGallery();
   }
 
   get items () {return this._items}
@@ -87,21 +30,22 @@ class Gallery {
   get defaultActiveItem () {return this._defaultActiveItem}
   get preview () {return this._preview}
 
-  
   createGallery() {
-    
     const img = document.createElement('img');
     const div = document.createElement('div');
+    const aRight = document.createElement('a');
+    const aLeft = document.createElement('a');
     
     div.classList.add('fullview-es6');
     div.classList.add('js-fullview-es6');
     img.classList.add('js-fullview-img-es6'); 
-
+    aLeft.classList.add('js-scroll-left');
+    aRight.classList.add('js-scroll-right');
     
     div.append(img);
     this.parentNode.prepend(div);
+    this.parentNode.append(aLeft, aRight);
 
-    
     this.items.map( el => {
       const img = document.createElement('img');
       img.setAttribute('src', el.preview);
@@ -114,25 +58,26 @@ class Gallery {
     });
 
     const fullviewImg = document.querySelector('.js-fullview-img-es6');
-    fullviewImg.setAttribute('src', this.items[this.defaultActiveItem].fullview);
-    
+    const scrollLeft = document.querySelector('.js-scroll-left');
+    const scrollRight = document.querySelector('.js-scroll-right');
+    const onScrolling = onScroll.bind(this);
+    fullviewImg.setAttribute('src', this.items[this.defaultActiveItem - 1].fullview);
+
     this.preview.addEventListener('click', onImgClick);
-    this.preview.addEventListener('mouseover', onImgOver);
-    this.preview.addEventListener('mouseout', onImgOut);
+    scrollLeft.addEventListener('click', onScrolling);
+    scrollRight.addEventListener('click', onScrolling);
     
     function onImgClick (e) { if ( e.target.nodeName === 'IMG') {fullviewImg.setAttribute ('src', e.target.getAttribute('data-fullview') )} }
-    function onImgOver (e) { if ( e.target.nodeName === 'IMG') {e.target.classList.toggle('js-current-img')} }
-    function onImgOut (e) { if ( e.target.nodeName === 'IMG') {e.target.classList.toggle('js-current-img')} }
+    function onScroll (e) {
+      e.target.classList.contains('js-scroll-left')
+      ? this.preview.scrollLeft -= 300
+      : this.preview.scrollLeft += 300;
+    }
   }
-  
 }
 
-const galleryPlugin = new Gallery({
+new Gallery({
   items: galleryItems,
   parentNode: document.querySelector('.image-gallery-es6'),
-  defaultActiveItem: 4
+  defaultActiveItem: 11
 });
-
-galleryPlugin.createGallery();
-
-/* Далее плагин работает в автономном режиме */
